7fcb68d030844fc98242ba017834316f29568786
Rojonantenaina-RAK <rojo.rak39@gmail.com>
2023-11-30 13:01:07 +0300

    🫡 Finition de tout le site, responsive

--- ressources/javascript/app.js
+++ ressources/javascript/app.js
@@ -1,8 +1,33 @@
-const btn_humberger = document.querySelector('.btn-show-nav');
+const btnHumberger = document.querySelector('.btn-show-nav');
 const header = document.querySelector('header');
-const all_navtop_links = document.querySelectorAll('header>nav a');
+const allNavtopLinks = document.querySelectorAll('header>nav a');
 
-btn_humberger.addEventListener('click', function(){header.classList.toggle('btn-active');});
-all_navtop_links.forEach(function(item){
+btnHumberger.addEventListener('click', function(){header.classList.toggle('btn-active');});
+allNavtopLinks.forEach(function(item){
     item.addEventListener('click', function(){header.classList.toggle('btn-active')});
 });
+
+
+function scrollToSectionWithDelay(event) {
+    event.preventDefault(); // Empêche le comportement par défaut du lien
+  
+    const targetId = event.currentTarget.getAttribute('href'); // Récupère l'ID de la cible depuis le lien
+    const targetSection = document.querySelector(targetId); // Trouve la section correspondante
+  
+    if (targetSection) {
+      setTimeout(() => {
+        const offsetTop = targetSection.offsetTop; // Récupère la position verticale de la section
+        window.scrollTo({
+          top: offsetTop,
+          behavior: 'smooth' // Active le défilement fluide
+        });
+      }, 100); // Attend une seconde (1000 millisecondes) avant de faire le scroll
+    }
+  }
+  
+  // Ajoute un gestionnaire d'événement de clic à tous les liens de navigation
+  const navLinks = document.querySelectorAll('nav a');
+  navLinks.forEach(link => {
+    link.addEventListener('click', scrollToSectionWithDelay);
+  });
+  
